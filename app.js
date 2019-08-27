7f5e72c84d70662c1f35ef8257c61e6335831a16
Arun Kumar <arun.kumar3@rivigo.com>
2019-08-28 01:16:15 +0530

    error handler fix

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.use('/phriction', phrictionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(400));
+  next(createError(404));
 });
 
-// default error handler
-app.use(function(err, req, res) {
-  //set locals, only providing error in development
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
