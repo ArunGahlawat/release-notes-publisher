2b5453c94f8b643947554149fcd0b6e6084e5244
Arun Kumar <arun.kumar3@rivigo.com>
2019-08-28 11:17:28 +0530

    removed debug log

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ if (process.env.MODE) {
 } else {
     process.argv.forEach(function (val, index, array) {
         var arg = val.split("=");
-        console.log("Got MODE=", process.env.MODE);
         if (arg.length > 0) {
             if (arg[0] === 'env') {
                 module.exports = require('./config/' + arg[1] + '.json');
