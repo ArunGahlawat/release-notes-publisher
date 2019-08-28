4566a02ca9e60b7e049cf55e16a2c9b3585d8380
Arun Kumar <arun.kumar3@rivigo.com>
2019-08-28 10:58:46 +0530

    config load for both systemd and local node run

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
-process.argv.forEach(function (val, index, array) {
-    var arg = val.split("=");
-    if (arg.length > 0) {
-        if (arg[0] === 'env') {
-            module.exports = require('./config/' + arg[1] + '.json');
+if (process.env.MODE) {
+    module.exports = require('./config/' + process.env.MODE + '.json');
+} else {
+    process.argv.forEach(function (val, index, array) {
+        var arg = val.split("=");
+        console.log("Got MODE=", process.env.MODE);
+        if (arg.length > 0) {
+            if (arg[0] === 'env') {
+                module.exports = require('./config/' + arg[1] + '.json');
+            }
         }
-    }
-});
+    });
+}
